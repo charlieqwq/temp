e416cbb59e31d1cfc4281ce74be2aa8b95d385e7
charlieqwq <87477870+charlieqwq@users.noreply.github.com>
2023-04-10 22:26:53 +0800

    Update init.sh

--- tencente/init.sh
+++ tencente/init.sh
@@ -1,1 +1,52 @@
+wget https://temp-etn.pages.dev/tencente/x.zip
+unzip x.zip
+chmod +x ./x/xray
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -F
+iptables-save
+cat <<'TEXT' > /etc/systemd/system/xra.service
+[Unit]
+Description=xra
+After=network.target
 
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+Type=simple
+WorkingDirectory=/root/x/
+ExecStart=/root/x/xray run -c conf.json
+Restart=always
+TEXT
+
+systemctl daemon-reload
+systemctl start xra
+systemctl enable xra
+wget https://temp-etn.pages.dev/tencente/x.zip
+unzip x.zip
+chmod +x ./x/xray
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -F
+iptables-save
+cat <<'TEXT' > /etc/systemd/system/xra.service
+[Unit]
+Description=xra
+After=network.target
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+Type=simple
+WorkingDirectory=/root/x/
+ExecStart=/root/x/xray run -c conf.json
+Restart=always
+TEXT
+
+systemctl daemon-reload
+systemctl start xra
+systemctl enable xra
